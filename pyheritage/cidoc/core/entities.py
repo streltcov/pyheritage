1bef07c8010460d0e0e31ef9e4a47e63467df21d
streltcov <streltsov83@yandex.ru>
2026-03-07 11:26:54 +0300

    update CRM entrypoint

--- pyheritage/cidoc/core/entities.py
+++ pyheritage/cidoc/core/entities.py
@@ -9,8 +9,23 @@ CRM entrypoint - all entities must be imported only from this module;
 
 from typing import Optional
 
-from pyheritage.cidoc.core._crm_base import E1CRMEntity, E2TemporalEntity, E3ConditionState
-from pyheritage.cidoc.core._frame import E52TimeeSpan, E53Place, E54Dimension
+from pyheritage.cidoc.core._crm_base import E1CRMEntity
+from pyheritage.cidoc.core._persistent import (
+    E18PhysicalThing,
+    E19PhysicalObject,
+    E20BiologicalObject,
+    E22HumanMadeObject,
+    E24PhysicalHumanMadeObject,
+    E25HumanMadeFeature,
+    E26PhysicalFeature,
+    E27Site,
+    E41Appellation,
+    E42Identifier,
+    E77PersistentItem,
+    E78CuratedHolding,
+    E90SymbolicObject,
+    E97MonetaryAmount,
+)
 from pyheritage.cidoc.core._primitives import (
     E59PrimitiveValue,
     E60Number,
@@ -19,10 +34,15 @@ from pyheritage.cidoc.core._primitives import (
     E94SpacePrimitive,
     E95SpaceTimePrimitive,
 )
+from pyheritage.cidoc.core._spacetime import E52TimeeSpan, E53Place, E54Dimension, E92SpaceTimeVolume, E93Presence
+from pyheritage.cidoc.core._temporal import E2TemporalEntity, E3ConditionState
 
 
-__all__ = ('E1CRMEntity', 'E2TemporalEntity', 'E3ConditionState', 'E59PrimitiveValue', 'E60Number',
-           'E61TimePrimitive', 'E62String', 'E94SpacePrimitive', 'E95SpaceTimePrimitive', )
+__all__ = ('E1CRMEntity', 'E2TemporalEntity', 'E3ConditionState', 'E18PhysicalThing', 'E19PhysicalObject',
+           'E20BiologicalObject', 'E22HumanMadeObject', 'E24PhysicalHumanMadeObject', 'E25HumanMadeFeature',
+           'E26PhysicalFeature', 'E27Site', 'E41Appellation', 'E42Identifier', 'E52TimeeSpan', 'E53Place',
+           'E54Dimension', 'E59PrimitiveValue', 'E60Number', 'E61TimePrimitive', 'E62String', 'E94SpacePrimitive',
+           'E95SpaceTimePrimitive', )
 
 
 __builtin_namespace__ = {
@@ -34,6 +54,16 @@ __namespace__ = {
     'E1CRMEntity': E1CRMEntity,
     'E2TemporalEntity': E2TemporalEntity,
     'E3ConditionState': E3ConditionState,
+    'E18PhysicalThing': E18PhysicalThing,
+    'E19PhysicalObject': E19PhysicalObject,
+    'E20BiologicalObject': E20BiologicalObject,
+    'E22HumanMadeObject': E22HumanMadeObject,
+    'E24PhysicalHumanMadeObject': E24PhysicalHumanMadeObject,
+    'E25HumanMadeFeature': E25HumanMadeFeature,
+    'E26PhysicalFeature': E26PhysicalFeature,
+    'E27Site': E27Site,
+    'E41Appellation': E41Appellation,
+    'E42Identifier': E42Identifier,
     'E52TimeSpan': E52TimeeSpan,
     'E53Place': E53Place,
     'E54Dimension': E54Dimension,
@@ -41,8 +71,14 @@ __namespace__ = {
     'E60Number': E60Number,
     'E61TimePrimitive': E61TimePrimitive,
     'E62String': E62String,
+    'E77PersistentItem': E77PersistentItem,
+    'E78CuratedHolding': E78CuratedHolding,
+    'E90SymbolicObject': E90SymbolicObject,
+    'E92SpacePrimitive': E92SpaceTimeVolume,
+    'E93Presence': E93Presence,
     'E94TimePrimitive': E94SpacePrimitive,
     'E95SpaceTimePrimitive': E95SpaceTimePrimitive,
+    'E97MonetaryAmount': E97MonetaryAmount,
 }
 
 
@@ -52,6 +88,16 @@ __namespace__.update(__builtin_namespace__)
 E1CRMEntity.model_rebuild(_types_namespace=__namespace__)
 E2TemporalEntity.model_rebuild(_types_namespace=__namespace__)
 E3ConditionState.model_rebuild(_types_namespace=__namespace__)
+E18PhysicalThing.model_rebuild(_types_namespace=__namespace__)
+E19PhysicalObject.model_rebuild(_types_namespace=__namespace__)
+E20BiologicalObject.model_rebuild(_types_namespace=__namespace__)
+E22HumanMadeObject.model_rebuild(_types_namespace=__namespace__)
+E24PhysicalHumanMadeObject.model_rebuild(_types_namespace=__namespace__)
+E25HumanMadeFeature.model_rebuild(_types_namespace=__namespace__)
+E26PhysicalFeature.model_rebuild(_types_namespace=__namespace__)
+E27Site.model_rebuild(_types_namespace=__namespace__)
+E41Appellation.model_rebuild(_types_namespace=__namespace__)
+E42Identifier.model_rebuild(_types_namespace=__namespace__)
 E52TimeeSpan.model_rebuild(_types_namespace=__namespace__)
 E53Place.model_rebuild(_types_namespace=__namespace__)
 E54Dimension.model_rebuild(_types_namespace=__namespace__)
@@ -59,5 +105,11 @@ E59PrimitiveValue.model_rebuild(_types_namespace=__namespace__)
 E60Number.model_rebuild(_types_namespace=__namespace__)
 E61TimePrimitive.model_rebuild(_types_namespace=__namespace__)
 E62String.model_rebuild(_types_namespace=__namespace__)
+E77PersistentItem.model_rebuild(_types_namespace=__namespace__)
+E78CuratedHolding.model_rebuild(_types_namespace=__namespace__)
+E90SymbolicObject.model_rebuild(_types_namespace=__namespace__)
+E92SpaceTimeVolume.model_rebuild(_types_namespace=__namespace__)
+E93Presence.model_rebuild(_types_namespace=__namespace__)
 E94SpacePrimitive.model_rebuild(_types_namespace=__namespace__)
 E95SpaceTimePrimitive.model_rebuild(_types_namespace=__namespace__)
+E97MonetaryAmount.model_rebuild(_types_namespace=__namespace__)
