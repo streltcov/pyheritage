87e91bb6a1a161c33981ba111d1f827fc8cc0955
streltcov <streltsov83@yandex.ru>
2026-03-10 23:26:24 +0300

    add p48, p102, p127, p137, p139 and p150 CRM properties (identification)

--- pyheritage/cidoc/core/properties/_identification.py
+++ pyheritage/cidoc/core/properties/_identification.py
@@ -36,7 +36,9 @@ if TYPE_CHECKING:
     from pyheritage.cidoc.core._primitives import CoercedString
 
 
-__all__ = ('P1IsIdentifiedBy', 'P2HasType', 'P3HasNote', 'P190HasSymbolicContent', )
+__all__ = ('P1IsIdentifiedBy', 'P2HasType', 'P3HasNote', 'P48HasPreferredIdentifier', 'P102HasTitle',
+           'P127HasBroaderTerm', 'P137Exemplifies', 'P139HasAlternativeForm', 'P150DefinesTypicalPartsOf',
+           'P190HasSymbolicContent', )
 
 
 class P1IsIdentifiedBy(PropertyMixin):
@@ -177,6 +179,278 @@ class P3HasNote(PropertyMixin):
     """
 
     p3_has_note: Optional[str] = Field(default=None, description='P3 has note')
+
+
+# ******************************************************************************************************************* #
+
+
+class P48HasPreferredIdentifier(PropertyMixin):
+    """'P48 has preferred identifier (is preferred identifier of)' CRM property;
+
+    https://cidoc-crm.org/html/cidoc_crm_v7.0.html#P48
+
+    Domain:
+        E1 CRM Entity
+    Range:
+        E42 Identifier
+    SubProperty Of:
+        E1 CRM Entity. P1 is identified by (identifies): E41 Appellation
+    SuperProperty Of:
+        -
+    Quantification:
+        many to one (0,1:0,n)
+
+    Scope Note:
+        This property records the preferred instance of E42 Identifier that was used to identify an instance of
+        E1 CRM Entity at the time this property was recorded;
+
+        More than one preferred identifier may have been assigned to an item over time;
+
+        Use of this property requires an external mechanism for assigning temporal validity to the respective
+        CIDOC CRM instance;
+
+        The fact that an identifier is a preferred one for an organisation can be better expressed in a context
+        independent form by assigning a suitable instance of E55 Type to the respective instance of
+        E15 Identifier Assignment using the P2 has type property;
+
+    Properties:
+        -
+    Examples:
+        - the pair of Lederhosen donated by Dr Martin Doerr (E22) has preferred identifier “OXCMS:2001.1.32” (E42)
+    In First Order Logic:
+        P48(x,y) ⊃ E1(x)
+        P48(x,y) ⊃ E42(y)
+        P48(x,y) ⊃ P1(x,y)
+
+    """
+
+    p48_has_preferred_identifier: Optional[str] = Field(
+        default=None,
+        description='P48 has preferred identifier (is preferred identifier of)'
+    )
+
+
+# ******************************************************************************************************************* #
+
+
+class P102HasTitle(PropertyMixin):
+    """'P102 has title (is title of)' CRM property;
+
+    https://cidoc-crm.org/html/cidoc_crm_v7.0.html#P102
+
+    Domain:
+        E71 Human-Made Thing
+    Range:
+        E35 Title
+    SubProperty Of:
+        E1 CRM Entity. P1 is identified by (identifies): E41 Appellation
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property associates an instance of E35 Title has been applied to an instance of E71 Human-Made Thing;
+
+        The P102.1 has type property of the P102 has title (is title of) property enables the relationship between the
+        title and the thing to be further clarified, for example, if the title was a given title, a supplied title etc.
+
+        It allows any human-made material or immaterial thing to be given a title. It is possible to imagine a title
+        being created without a specific object in mind;
+
+    Properties:
+        P102.1 has type: E55 Type
+    Examples:
+        - the first book of the Old Testament (E33) has title “Genesis” (E35)
+        - has type translated (E55)
+    In First Order Logic:
+        P102(x,y) ⊃ E71(x)
+        P102(x,y) ⊃ E35(y)
+        P102(x,y,z) ⊃ [P102(x,y) ∧ E55(z)]
+        P102(x,y) ⊃ P1(x,y)
+
+    """
+
+    p102_has_title: Optional[str] = Field(default=None, description='P102 has title (is title of)')
+
+
+# ******************************************************************************************************************* #
+
+
+class P127HasBroaderTerm(PropertyMixin):
+    """'P127 has broader term (has narrower term)' CRM property;
+
+    https://cidoc-crm.org/html/cidoc_crm_v7.0.html#P127
+
+    Domain:
+        E55 Type
+    Range:
+        E55 Type
+    SubProperty Of:
+        -
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property associates an instance of E55 Type with another instance of E55 Type that has a broader meaning;
+
+        It allows instances of E55 Types to be organised into hierarchies. This is the sense of "broader term generic
+        (BTG)" as defined in ISO 25964-2:2013;
+
+        This property is transitive;
+
+    Properties:
+        -
+    Examples:
+        - dime (E55) has broader term coin (E55)
+    In First Order Logic:
+        P127(x,y) ⊃ E55(x)
+        P127(x,y) ⊃ E55(y)
+
+    """
+
+    p127_has_broader_term: Optional[str] = Field(
+        default=None,
+        description='P127 has broader term (has narrower term)'
+    )
+
+
+# ******************************************************************************************************************* #
+
+
+class P137Exemplifies(PropertyMixin):
+    """'P137 exemplifies (is exemplified by)' CRM property;
+
+    https://cidoc-crm.org/html/cidoc_crm_v7.0.html#P137
+
+    Domain:
+        E1 CRM Entity
+    Range:
+        E55 Type
+    SubProperty Of:
+        E1 CRM Entity. P2 has type (is type of): E55 Type
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property associates an instance of E1 CRM Entity with an instance of E55 Type for which it has been
+        declared to be a particularly characteristic example;
+
+        The P137.1 in the taxonomic role property of P137 exemplifies (is exemplified by) allows differentiation of
+        taxonomic roles. The taxonomic role renders the specific relationship of this example to the type, such as
+        "prototypical", "archetypical", "lectotype", etc. The taxonomic role "lectotype" is not associated with the
+        instance of E83 Type Creation itself, but selected in a later phase;
+
+    Properties:
+        P137.1 in the taxonomic role: E55 Type
+    Examples:
+        - Object BM000098044 of the Clayton Herbarium (E20) exemplifies Spigelia marilandica (L.) L. (E55) in the
+          taxonomic role lectotype
+    In First Order Logic:
+        P137(x,y) ⊃ E1(x)
+        P137(x,y) ⊃ E55(y)
+        P137(x,y,z) ⊃ [P137(x,y) ∧ E55(z)]
+        P137(x,y) ⊃ P2(x,y)
+
+    """
+
+    p137_exemplifies: Optional[str] = Field(default=None, description='P137 exemplifies (is exemplified by)')
+
+
+# ******************************************************************************************************************* #
+
+
+class P139HasAlternativeForm(PropertyMixin):
+    """'P139 has alternative form' CRM property;
+
+    https://cidoc-crm.org/html/cidoc_crm_v7.0.html#P139
+
+    Domain:
+        E41 Appellation
+    Range:
+        E41 Appellation
+    SubProperty Of:
+        -
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property establishes a relationship of equivalence between two instances of E41 Appellation independent
+        from any item identified by them. It is a dynamic asymmetric relationship, where the range expresses the
+        derivative, if such a direction can be established. Otherwise, the relationship is symmetric. The relationship
+        is not transitive;
+
+        The equivalence applies to all cases of use of an instance of E41 Appellation. Multiple names assigned to an
+        object, which are not equivalent for all things identified with a specific instance of E41 Appellation, should
+        be modelled as repeated values of P1 is identified by (identifies);
+
+        P139.1 has type allows the type of derivation, such as “transliteration from Latin 1 to ASCII” be refined..
+
+    Properties:
+        P139.1 has type: E55 Type
+    Examples:
+        - "Martin Doerr" (E41) has alternative form "Martin Dörr" (E41) has type Alternate spelling (E55)
+        - "Гончарова, Наталья Сергеевна" (E41) has alternative form "Gončarova, Natal´â Sergeevna" (E41) has type
+          ISO 9:1995 transliteration (E55)
+        - “Αθήνα” has alternative form “Athina” has type transcription.
+    In First Order Logic:
+        P139(x,y) ⊃ E41(x)
+        P139 (x,y) ⊃ E41(y)
+        P139(x,y,z) ⊃ [P139(x,y) ∧ E55(z)]
+        P139(x,y) ⊃ P139(y,x)
+
+    """
+
+    p139_has_alternative_form: Optional[str] = Field(default=None, description='P139 has alternative form')
+
+
+# ******************************************************************************************************************* #
+
+
+class P150DefinesTypicalPartsOf(PropertyMixin):
+    """'P150 defines typical parts of (defines typical wholes for)' CRM property;
+
+    https://cidoc-crm.org/html/cidoc_crm_v7.0.html#P150
+
+    Domain:
+        E55 Type
+    Range:
+        E55 Type
+    SubProperty Of:
+        -
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property associates an instance of E55 Type “A” with an instance of E55 Type “B”, when items of type “A”
+        typically form part of items of type “B”, such as “car motors” and “cars”. The property is not transitive;
+
+        It allows types to be organised into hierarchies based on one type describing a typical part of another. This
+        property is equivalent to "broader term partitive (BTP)" as defined in ISO 2788 and “broaderPartitive”
+        in SKOS;
+
+    Properties:
+        -
+    Examples:
+        - Car motors (E55) defines typical parts of cars (E55)
+    In First Order Logic:
+        P150(x,y) ⊃ E55(x)
+        P150(x,y) ⊃ E55(y)
+
+    """
+
+    p150_defines_typical_parts_of: Optional[str] = Field(
+        default=None,
+        description='P150 defines typical parts of (defines typical wholes for)'
+    )
 
 
 # ******************************************************************************************************************* #
