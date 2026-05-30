5e5f734c603a3e97ae62113c185f34650c72b7ec
streltcov <streltsov83@yandex.ru>
2026-05-30 23:45:51 +0300

    add crmarchaeo package

---
+++ pyheritage/cidoc/crmarchaeo/entities.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""CRMArchaeo entity models;
+
+CRMArchaeo v2.0
+
+Entities
+--------
+A1 Excavation Processing Unit
+A2 Stratigraphic Volume Unit
+A3 Stratigraphic Interface
+A4 Stratigraphic Genesis
+A5 Stratigraphic Modification
+A6 Group Declaration Event
+A7 Embedding
+A8 Stratigraphic Unit
+A9 Archaeological Excavation
+A10 Excavation Interface
+
+"""
---
+++ pyheritage/cidoc/crmarchaeo/properties.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""CRMArchaeo property models;
+
+(Mixin classes for entity models);
+
+CRMArchaeo v2.0
+
+------------------------------------------------
+Properties
+------------------------------------------------
+AP1  produced                          A1  -> S11
+AP2  discarded                         A1  -> S11
+AP3  investigated                      A9  -> E27
+AP4  produced surface                  A1  -> A10
+AP5  removed part or all of            A1  -> A8
+AP6  intended to approximate           A1  -> A3
+AP7  produced                          A4  -> A8
+AP8  disturbed                         A5  -> A8
+AP9  took matter from                  A4  -> S10
+AP10 destroyed                         A1  -> S22
+AP11 has physical relation to          A8  -> A8
+AP12 confines                          A3  -> A2
+AP13 has stratigraphic relation to     A5  -> A5
+AP15 is or contains remains of         A2  -> S10
+AP16 assigned attribute to             A6  -> E18
+AP17 is found by                       A7  -> S19
+AP18 is embedding of                   A7  -> E18
+AP19 is embedding in                   A7  -> A2
+AP21 contains                          A2  -> E18
+AP22 is equal in time to               E2  -> E2
+AP23 finishes                          E2  -> E2
+AP24 starts                            E2  -> E2
+AP25 occurs during                     E2  -> E2
+AP26 overlaps in time with             E2  -> E2
+AP27 meets in time with                E2  -> E2
+AP28 occurs before                     E2  -> E2
+AP29 appears in                        E55 -> E4
+AP30 restricted to                     E55 -> E4
+AP31 typical for                       E55 -> E4
+AP32 discarded into                    A1  -> S11
+
+"""
