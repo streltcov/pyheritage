602a1aef34f6be7ea713b90293486f80f07d0887
streltcov <streltsov83@yandex.ru>
2026-06-03 23:43:38 +0300

    add CRMArchaeo AP1-AP5 properties

--- pyheritage/cidoc/crmarchaeo/properties.py
+++ pyheritage/cidoc/crmarchaeo/properties.py
@@ -41,3 +41,229 @@ AP31 typical for                       E55 -> E4
 AP32 discarded into                    A1  -> S11
 
 """
+
+
+from __future__ import annotations
+
+from typing import List, TYPE_CHECKING
+
+from pydantic import Field
+
+from pyheritage.cidoc.base import PropertyMixin
+
+
+if TYPE_CHECKING:
+    from pyheritage.cidoc.core.entities import E27Site
+    from pyheritage.cidoc.crmarchaeo.entities import (
+        A8StratigraphicUnit,
+        A10ExcavationInterface,
+    )
+    from pyheritage.cidoc.crmsci.entities import S11AmountOfMatter
+
+
+__all__ = (
+    'AP1Produced',
+    'AP2Discarded',
+    'AP3Investigated',
+    'AP4ProducedSurface',
+    'AP5RemovedPartOrAll',
+)
+
+
+class AP1Produced(PropertyMixin):
+    """'AP1 produced (was produced by)' CRMArchaeo property;
+
+    https://cidoc-crm.org/extensions/crmarchaeo/html/CRMarchaeo_v2.0.html#AP1
+
+    Domain:
+        A1 Excavation Processing Unit
+    Range:
+        S11 Amount of Matter
+    SubProperty Of:
+        S1 Matter Removal. O2 removed (was removed by): S11 Amount of Matter
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property associates an instance of A1 Excavation Processing Unit with an
+        instance of S11 Amount of Matter that was produced and documented during this
+        excavation activity. The produced matter comprises finds, samples, and other
+        material that the excavator separates for further analysis or curation.
+
+    Properties:
+        -
+    Examples:
+        -
+    In First Order Logic:
+        AP1(x,y) ⊃ A1(x)
+        AP1(x,y) ⊃ S11(y)
+
+    """
+
+    ap1_produced: List[S11AmountOfMatter] = Field(
+        default=None,
+        description='AP1 produced (was produced by)',
+    )
+
+
+# ******************************************************************************************************************* #
+
+
+class AP2Discarded(PropertyMixin):
+    """'AP2 discarded (was discarded by)' CRMArchaeo property;
+
+    https://cidoc-crm.org/extensions/crmarchaeo/html/CRMarchaeo_v2.0.html#AP2
+
+    Domain:
+        A1 Excavation Processing Unit
+    Range:
+        S11 Amount of Matter
+    SubProperty Of:
+        S1 Matter Removal. O2 removed (was removed by): S11 Amount of Matter
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property associates an instance of A1 Excavation Processing Unit with the
+        instance of S11 Amount of Matter that was discarded during this activity.
+        Discarded matter is removed but not kept for further analysis.
+
+    Properties:
+        -
+    Examples:
+        -
+    In First Order Logic:
+        AP2(x,y) ⊃ A1(x)
+        AP2(x,y) ⊃ S11(y)
+
+    """
+
+    ap2_discarded: List[S11AmountOfMatter] = Field(
+        default=None,
+        description='AP2 discarded (was discarded by)',
+    )
+
+
+# ******************************************************************************************************************* #
+
+
+class AP3Investigated(PropertyMixin):
+    """'AP3 investigated (was investigated by)' CRMArchaeo property;
+
+    https://cidoc-crm.org/extensions/crmarchaeo/html/CRMarchaeo_v2.0.html#AP3
+
+    Domain:
+        A9 Archaeological Excavation
+    Range:
+        E27 Site
+    SubProperty Of:
+        E7 Activity. P7 took place at (witnessed): E53 Place
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property associates an instance of A9 Archaeological Excavation with the
+        instance of E27 Site that is being investigated by this excavation.
+
+    Properties:
+        -
+    Examples:
+        -
+    In First Order Logic:
+        AP3(x,y) ⊃ A9(x)
+        AP3(x,y) ⊃ E27(y)
+
+    """
+
+    ap3_investigated: List[E27Site] = Field(
+        default=None,
+        description='AP3 investigated (was investigated by)',
+    )
+
+
+# ******************************************************************************************************************* #
+
+
+class AP4ProducedSurface(PropertyMixin):
+    """'AP4 produced surface (was surface produced by)' CRMArchaeo property;
+
+    https://cidoc-crm.org/extensions/crmarchaeo/html/CRMarchaeo_v2.0.html#AP4
+
+    Domain:
+        A1 Excavation Processing Unit
+    Range:
+        A10 Excavation Interface
+    SubProperty Of:
+        E12 Production. P108 has produced (was produced by): E24 Physical Human-Made Thing
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many (0,n:0,n)
+
+    Scope Note:
+        This property associates an instance of A1 Excavation Processing Unit with an
+        instance of A10 Excavation Interface that was produced by the excavation activity.
+        The excavation interface is the surface (planum, profile, etc.) resulting from
+        the removal of matter.
+
+    Properties:
+        -
+    Examples:
+        -
+    In First Order Logic:
+        AP4(x,y) ⊃ A1(x)
+        AP4(x,y) ⊃ A10(y)
+
+    """
+
+    ap4_produced_surface: List[A10ExcavationInterface] = Field(
+        default=None,
+        description='AP4 produced surface (was surface produced by)',
+    )
+
+
+# ******************************************************************************************************************* #
+
+
+class AP5RemovedPartOrAll(PropertyMixin):
+    """'AP5 removed part or all of (was partially or totally removed by)' CRMArchaeo property;
+
+    https://cidoc-crm.org/extensions/crmarchaeo/html/CRMarchaeo_v2.0.html#AP5
+
+    Domain:
+        A1 Excavation Processing Unit
+    Range:
+        A8 Stratigraphic Unit
+    SubProperty Of:
+        S1 Matter Removal. O1 diminished (was diminished by): S10 Material Substantial
+    SuperProperty Of:
+        -
+    Quantification:
+        many to many, necessary (1,n:0,n)
+
+    Scope Note:
+        This property associates an instance of A1 Excavation Processing Unit with the
+        instance of A8 Stratigraphic Unit that was entirely or partially removed during
+        this excavation activity.
+
+    Properties:
+        -
+    Examples:
+        -
+    In First Order Logic:
+        AP5(x,y) ⊃ A1(x)
+        AP5(x,y) ⊃ A8(y)
+
+    """
+
+    ap5_removed_part_or_all: List[A8StratigraphicUnit] = Field(
+        default=None,
+        min_length=1,
+        description='AP5 removed part or all of (was partially or totally removed by)',
+    )
