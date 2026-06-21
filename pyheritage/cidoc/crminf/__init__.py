e2ce8f78b562cb1afeab8acef0b8f24c685159a2
streltcov <streltsov83@yandex.ru>
2026-06-21 18:35:30 +0300

    add crminf package

---
+++ pyheritage/cidoc/crminf/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""CRMinf extension models package;
+
+CRMinf v1.0 (released October 2023)
+
+https://www.cidoc-crm.org/crminf/ModelVersion/version-1.0-1
+
+"""
---
+++ pyheritage/cidoc/crminf/entities.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""CRMinf entity models;
+
+CRMinf v1.0
+
+Entities
+--------
+I1  Argumentation
+I2  Belief
+I3  Inference Logic
+I4  Proposition Set
+I5  Inference Making
+I6  Belief Value
+I7  Belief Adoption
+I10 Provenance Statement
+I11 Situation
+I12 Adopted Belief
+I13 Intended Meaning Belief
+I14 Provenance Belief
+I15 Provenance Assessment
+I16 Meaning Comprehension
+I17 Categorical Hypothesis Building
+
+"""
